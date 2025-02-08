0eb4abc9b526d41bcebc1330ba41ce00e87305cd
Bototep <witchaponthanaphat@gmail.com>
2025-02-08 07:17:27 +0700

    forgot to update little fix

--- Assets/Scripts/AnimationStrings.cs
+++
@@ -1,8 +0,0 @@
-internal class AnimationStrings 
-{
-    internal static string isMoving = "IsMoving";
-    internal static string isGrounded = "IsGrounded";
-    internal static string jump = "Jump";
-    internal static string isOnWall = "IsOnWall";
-    internal static string isOnCeiling = "IsOnCeiling";
-}
